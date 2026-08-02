8c552ac394dc100933063e9d082d891ac03940e1
mehdi7abdi <mehdihca@outlook.fr>
2026-08-02 02:02:56 +0100

    Ajout du reducer navbar au store pour gérer l'état du menu de navigation

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "../features/theme/themeSlice";
 import heroCarouselReducer from "../features/heroCarousel/heroCarouselSlice";
+import navbarReducer from "../features/navbar/navbarSlice";
 import { api } from "@/server/api";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     heroCarousel: heroCarouselReducer,
+    navbar: navbarReducer,
     [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
